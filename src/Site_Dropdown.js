f38157a00cbf89fc6033d6a0f1a1d05c9e6ff613
jrogers-dev <JRogers.dev@gmail.com>
2021-06-28 20:19:17 -0700

    Filled in dropdown component basic code

--- src/Site_Dropdown.js
+++ src/Site_Dropdown.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Dropdown } from '@bit/primefaces.primereact.dropdown';
+import PrimereactStyle from '@bit/primefaces.primereact.internal.stylelinks';
 
-function Site_Dropdown() {
-  return (
-    <div>
-      
-    </div>
-  );
+class Site_Dropdown extends React.Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+			storeSelectItems: [
+				{ label: 'Home Depot', value: '' },
+				{ label: 'Lowes', value: '' },
+				{ label: 'Target', value: '' },
+				{ label: 'Walmart', value: '' }
+			],
+			store: ''
+		};
+  }
+  
+  render() {
+    return (
+      <div>
+        <PrimereactStyle />
+				<Dropdown
+					style={{ width: 150 }}
+					value={this.state.store}
+					options={this.state.storeSelectItems}
+					onChange={e => {
+						this.setState({ store: e.value });
+					}}
+					placeholder='Select a Store'
+				/>
+      </div>
+    );
+  }
 }
 
 export default Site_Dropdown;
