70bc100ba310636b54d37629d2f17b66278f2902
jrogers-dev <JRogers.dev@gmail.com>
2021-07-01 03:54:59 -0700

    Installed React Data Grid module and inserted a basic grid into the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SiteDropdown from './SiteDropdown';
+import Results from './Results';
 
 function App() {
   return (
     <div className="App">
       <SiteDropdown />
+      <Results />
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import ReactDataGrid from 'react-data-grid';
 
-function Results() {
-  return (
-    <div>
-      
-    </div>
-  );
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' },
+  { key: 'count', name: 'Count' } ];
+
+const rows = [{id: 0, title: 'row1', count: 20}, {id: 1, title: 'row1', count: 40}, {id: 2, title: 'row1', count: 60}];
+
+class Results extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactDataGrid
+          rows={rows}
+          columns={columns}
+          minHeight={150} 
+        />
+      </div>
+    );
+  }
 }
 
 export default Results;
