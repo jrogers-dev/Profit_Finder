266f70d22a8314b5eadd53d0197e28ea790d19f3
jrogers-dev <JRogers.dev@gmail.com>
2021-07-01 01:29:30 -0700

    Fixed errors: Installed modules stylelinks and classnames, syntax error in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import SiteDropdown from './SiteDropdown';'./SiteDropdown'
+import SiteDropdown from './SiteDropdown';
 
 function App() {
   return (
