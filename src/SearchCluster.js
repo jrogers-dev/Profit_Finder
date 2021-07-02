ba5547bc233c957b83870609160eec05b866b05e
jrogers-dev <JRogers.dev@gmail.com>
2021-07-02 00:45:52 -0700

    Utilized react-bootstraps row and col to give a basic layout to SearchCluster with a placeholder search box for now

--- src/SearchCluster.js
+++ src/SearchCluster.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import SiteDropdown from './SiteDropdown';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class SearchCluster extends React.Component {
   render() {
     return (
-      <div>
-        <SiteDropdown/>
-        <Button/>
-      </div>
+      <>
+      <Container>
+        <Row>
+          <Col>
+            <input type="text"></input>
+          </Col>
+          <Col>
+            <SiteDropdown/>
+          </Col>
+          <Col>
+            <Button/>
+          </Col>
+        </Row>
+      </Container>
+      </>
     );
   }
 
